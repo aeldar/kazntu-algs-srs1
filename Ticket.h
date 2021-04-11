27b3bf90594a8e8306a40ed01f33e9f945a75846
aeldar <eldar.aliyev8@gmail.com>
2021-04-11 21:17:44 +0500

    add parametrized constructor

--- Ticket.h
+++ Ticket.h
@@ -15,21 +15,40 @@ private:
 
 public:
   Ticket();
+  Ticket(int route_no, string point_of_departure, string departure_time,
+      string destination, string arrival_time, double price);
   string to_string();
-  friend ostream& operator<< (ostream& os, Ticket t);
+  friend ostream& operator<<(ostream& os, Ticket t);
 };
 
-Ticket::Ticket () {
+// ----- implementation -----
+
+Ticket::Ticket() {
   route_no = 0;
-  point_of_departure = "";
-  departure_time = "";
-  destination = "";
-  arrival_time = "";
+  point_of_departure = "---";
+  departure_time = "--/--/--";
+  destination = "---";
+  arrival_time = "--/--/--";
   price = 0.0;
 }
 
+Ticket::Ticket(int route_no, string point_of_departure, string departure_time,
+    string destination, string arrival_time, double price) {
+  this->route_no = route_no;
+  this->point_of_departure = point_of_departure;
+  this->departure_time = departure_time;
+  this->destination = destination;
+  this->arrival_time = arrival_time;
+  this->price = price;
+}
+
 string Ticket::to_string () {
-  return route_no + " " + point_of_departure + " " +  departure_time;
+  return std::to_string(route_no)
+    + " " + point_of_departure
+    + " " + departure_time
+    + " " + destination
+    + " " + arrival_time
+    + " " + std::to_string(price);
 }
 
 ostream& operator<< (ostream& os, Ticket t) {
--- main.cpp
+++ main.cpp
@@ -6,6 +6,15 @@ using namespace std;
 
 int main() {
   Ticket t;
+  Ticket tx[10];
+
+  cout << sizeof(tx)/sizeof(Ticket[1]) << endl;
+
+  tx[0] = Ticket(1, "fdsfd", "afaf", "fdfd", "asda", 123.54);
+
+  for (int i = 0; i < 10; i++) {
+    cout << tx[i] << endl;
+  }
 
   cout << t << endl;
 
