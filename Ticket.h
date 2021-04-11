eeb81e13d0bfd975a90ceb71436639018860d23d
aeldar <eldar.aliyev8@gmail.com>
2021-04-11 20:21:35 +0500

    add default constructor

--- Ticket.h
+++ Ticket.h
@@ -13,8 +13,18 @@ private:
   double price;
 
 public:
+  Ticket();
   string to_string();
 };
+
+Ticket::Ticket () {
+  route_no = 0;
+  point_of_departure = "";
+  departure_time = "";
+  destination = "";
+  arrival_time = "";
+  price = 0.0;
+}
 
 string Ticket::to_string () {
   return route_no + " " + point_of_departure + " " +  departure_time;
