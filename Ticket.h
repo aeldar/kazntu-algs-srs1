d4502f900f62fd07f9a09d96b734c88cc69c28b5
aeldar <eldar.aliyev8@gmail.com>
2021-04-11 19:58:47 +0500

    add ticket.h

---
+++ Ticket.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+class Ticket {
+private:
+  int route_no;
+  string point_of_departure;
+  string departure_time;
+  string destination;
+  string arrival_time;
+  double price;
+
+public:
+
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "Ticket.h"
+
+using namespace std;
+
+int main() {
+  Ticket t;
+
+  cout << t.route_no << t.point_of_departure << endl;
+
+  return 0;
+}
