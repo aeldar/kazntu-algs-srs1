da238c757ba4042ad0412e1a55ebaf3c69434829
aeldar <eldar.aliyev8@gmail.com>
2021-05-19 16:22:00 +0500

    add menu

--- app.h
+++ app.h
@@ -5,26 +5,69 @@ using namespace std;
 
 int top_menu() {
 
-  int menu_option;
+  int selected_option;
   bool done = false;
 
   while (!done) {
 
     // show menu
-    cout << "Choose option" << endl;
-    cout << "1 - sort, 2 - filter, 3 - reset" << endl;
-    cout << "? ";
-    cin >> menu_option;
+    cout << endl;
+    cout << "Choose option:" << endl;
+    cout << " 1 - sort" << endl;
+    cout << " 2 - filter" << endl;
+    cout << " 3 - reset" << endl;
+    cout << " 4 - exit" << endl;
+    cout << " ? ";
+    cin >> selected_option;
 
-    cout << "SELECTED: " << menu_option << endl;
+    // debug
+    cout << "SELECTED: " << selected_option << endl;
 
-    if (menu_option >= 1 && menu_option <= 3) {
+    if (selected_option >= 1 && selected_option <= 4) {
       done = true;
     } else {
-      cout << "Wrong option." << endl;
+      cout << "Wrong option. Try again." << endl;
     }
   }
 
-  return menu_option;
+  return selected_option;
+}
+
+int sort_menu() {
+
+  int selected_option;
+
+  cout << endl;
+  cout << "How to sort?" << endl;
+  cout << " 1 - by Route No" << endl;
+  cout << " 2 - by Point of Departure" << endl;
+  cout << " 3 - by Destination" << endl;
+  cout << " 4 - by Departure Time" << endl;
+  cout << " 5 - by Arrival Time" << endl;
+  cout << " 6 - by Price" << endl;
+  cout << " ? ";
+
+  bool done = false;
+  while(!done) {
+    cin >> selected_option;
+    if (selected_option >= 1 && selected_option <= 6) {
+      done = true;
+    } else {
+      cout << "Wrong option. Try again." << endl;
+    }
+  }
+
+  return selected_option;
+}
+
+string filter_menu() {
+
+  string filter_string = "";
+
+  cout << endl;
+  cout << "Enter a string to search for: ";
+  cin >> filter_string;
+
+  return filter_string;
 }
 
--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ using namespace std;
 
 int main() {
   std::vector<Ticket> v;
+  int sort_option = 1;
+  string filter_string = "";
 
   // read file "data.txt" to fill in vector with actual data
   Ticket t;
@@ -30,10 +32,30 @@ int main() {
   }
 
   int top_menu_option = top_menu();
-  cout << "selected option: " << top_menu_option;
-  // int kuku = 0;
-  // cin >> kuku;
-  // cout << "KUKU: " << kuku;
+  cout << "selected option: " << top_menu_option << endl;
+  
+  switch (top_menu_option) {
+    case 1:
+      sort_option = sort_menu();
+      break;
+    case 2:
+      filter_string = filter_menu();
+      break;
+    case 3:
+      sort_option = 1;
+      filter_string = "";
+      break;
+    case 4:
+      // exit the app
+      return 0;
+    default:
+      cerr << "Wrong option. Or not yet implemented. Please restart.";
+      break;
+  }
+
+  // debug
+  cout << "Sorting option: " << sort_option << endl;
+  cout << "Filter string: " << filter_string << endl;
 
   return 0;
 }
